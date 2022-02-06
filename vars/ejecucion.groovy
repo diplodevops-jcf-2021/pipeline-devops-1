515e8f9cfd26fd00416d763f33edf84ea7c949ee
Nelson Agurto <nelsonagurtop@gmail.com>
2022-02-06 11:21:18 -0400

    Actualizo Mensajeria Slack

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -20,10 +20,12 @@ void call(String buildTool = 'maven') {
         }
         post {
             success {
-                slackSend(color: '#00FF00', message: '[gamboa][' + env.JOB_NAME + '][' + buildTool + '] Ejecución Exitosa.')
+                //slackSend(color: '#00FF00', message: '[gamboa][' + env.JOB_NAME + '][' + buildTool + '] Ejecución Exitosa.')
+                slackSend color: 'good', message: "[Secc2-Grp4][Pipeline: ${env.buildTool}][Rama: ${env.BRANCH_NAME}][Stage:${CURRENT_STAGE}] Ejecucion exitosa."
             }
             failure {
-                slackSend(color: '#FF0000', message: '[gamboa][' + env.JOB_NAME + '][' + buildTool + '] Ejecución Fallida en Stage [' + env.CURRENT_STAGE + '].')
+                //slackSend(color: '#FF0000', message: '[gamboa][' + env.JOB_NAME + '][' + buildTool + '] Ejecución Fallida en Stage [' + env.CURRENT_STAGE + '].')
+                slackSend color: 'danger', message: "[Secc2-Grp4][Pipeline: ${env.buildTool}][Rama: ${env.BRANCH_NAME}] Ejecucion fallida en stage [${CURRENT_STAGE}]."
             }
         }
     }
