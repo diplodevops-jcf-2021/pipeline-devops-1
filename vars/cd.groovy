319cb83bec75bd3112508ee120617e05612262b8
ngamboa <ngamboa@insacom.cl>
2022-02-07 17:03:50 -0300

    actualizo cd

--- vars/cd.groovy
+++ vars/cd.groovy
@@ -40,16 +40,16 @@ void call() {
     if (currentStages.contains(nexusDownload)) {
         stage(nexusDownload) {
 
-            env.ARTIFACT_GROUP_ID  = sh script: './mvnw help:evaluate -Dexpression=project.groupId -q -DforceStdout', returnStdout: true
-            env.ARTIFACT_ID  = sh script: './mvnw help:evaluate -Dexpression=project.artifactId -q -DforceStdout', returnStdout: true
-            env.ARTIFACT_VERSION  = sh script: './mvnw help:evaluate -Dexpression=project.version -q -DforceStdout', returnStdout: true
+            String ARTIFACT_GROUP_ID  = sh script: './mvnw help:evaluate -Dexpression=project.groupId -q -DforceStdout', returnStdout: true
+            String ARTIFACT_ID  = sh script: './mvnw help:evaluate -Dexpression=project.artifactId -q -DforceStdout', returnStdout: true
+            String ARTIFACT_VERSION  = sh script: './mvnw help:evaluate -Dexpression=project.version -q -DforceStdout', returnStdout: true
 
             CURRENT_STAGE = nexusDownload
             figlet CURRENT_STAGE
             withCredentials([usernameColonPassword(credentialsId: env.NEXUS_CREDENTIALS_ID, variable: 'NEXUS_CREDENTIALS')]) {
-                String repoUrl = "${env.NEXUS_URL}/repository/${env.NEXUS_REPOSITORY_ID}/${env.ARTIFACT_GROUP_ID}/${env.ARTIFACT_ID}/${env.ARTIFACT_VERSION}/${env.ARTIFACT_ID}-${env.ARTIFACT_VERSION}.jar";
+                String repoUrl = "${env.NEXUS_URL}/repository/${env.NEXUS_REPOSITORY_ID}/${ARTIFACT_GROUP_ID}/${ARTIFACT_ID}/${ARTIFACT_VERSION}/${ARTIFACT_ID}-${ARTIFACT_VERSION}.jar";
                 println repoUrl
-                sh 'curl -u ${NEXUS_CREDENTIALS} "' + repoUrl + '" -O'
+                sh 'curl -u ${NEXUS_CREDENTIALS} "' + repoUrl + '" -o app.jar'
             }
         }
     }
@@ -57,12 +57,9 @@ void call() {
     // run
     if (currentStages.contains(run)) {
         stage(run) {
-            env.ARTIFACT_GROUP_ID  = sh script: './mvnw help:evaluate -Dexpression=project.groupId -q -DforceStdout', returnStdout: true
-            env.ARTIFACT_ID  = sh script: './mvnw help:evaluate -Dexpression=project.artifactId -q -DforceStdout', returnStdout: true
-            env.ARTIFACT_VERSION  = sh script: './mvnw help:evaluate -Dexpression=project.version -q -DforceStdout', returnStdout: true
             CURRENT_STAGE = run
             figlet CURRENT_STAGE
-            sh "java -jar ${env.ARTIFACT_ID}-${env.ARTIFACT_VERSION}.jar &"
+            sh "java -jar app.jar &"
             sleep 20
         }
     }
